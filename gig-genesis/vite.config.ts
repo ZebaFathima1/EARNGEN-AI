8792c353c0a6b2ef05b401127c3f747ea8be0aaa
ZebaFathima1 <fathimazeba882@gmail.com>
2026-05-14 19:38:31 +0530

    changes made

--- gig-genesis/vite.config.ts
+++ gig-genesis/vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { defineConfig, loadEnv, mergeConfig } from "vite";
 import type { PluginOption } from "vite";
@@ -19,6 +20,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     }),
+    nitro(),
     viteReact(),
   ];
 
